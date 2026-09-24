14b8b3d793edddd5234824331a9a49fb278f6f85
siddache <siddache@gmail.com>
2026-09-24 13:49:48 +0530

    auto: update C-programminig/pmadadweew.c, C-programminig/pmax.c

--- C-programminig/pmadadweew.c
+++
@@ -1,5 +0,0 @@
-//awdawdwdwdaw judhhu jif u higsacu g ahsgf  hhvf sug jiasbsficug asuigf  i ihgasfuig jgga sfcgo hhgsaf v viasfhg
-#include <stdio.h>
-int main (){
-    printf("thanks pmax for this bat file  ");
-}
