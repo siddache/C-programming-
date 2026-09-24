dc8f62d8c988d51087c18734c1ffbc5bd2bcd17a
siddache <siddache@gmail.com>
2026-09-24 17:12:10 +0530

    auto: update xoryaxis.c

--- xoryaxis.c
+++ xoryaxis.c
@@ -2,18 +2,23 @@
 #include <stdio.h>
 int main (){
     int x,y;
-    printf("enter the value of x and y : ");
-      scanf("%d %d ",&x , &y);
+    printf("enter the value of x  : ");
+    scanf("%d ",&x);
+
+    printf("enter the value of y  : ");
+    scanf("%d ",&y);
+
+
 
       if(y==0 && x==0){
         printf(" lies on origin  ");
       }
-      else if (x==0){
-        printf("lies on y axis  ");
+      else if (y==0){
+        printf("lies on x axis  ");
       }
-      else if (y==0)
+      else if (x==0)
         {
-            printf("lies on x axis ");
+            printf("lies on y axis ");
         }
         else{
             printf("neither on the axes nor at the origin");
